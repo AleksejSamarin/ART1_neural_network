d094dcfbb90b018a1c35da9b9947d26d2c66f90f
Aleksej <samarin113@gmail.com>
2019-03-18 15:28:44 +0000

    Redact imports

--- Main.py
+++ Main.py
@@ -1,6 +1,4 @@
-from scripts.NetworkART1 import *
-from scripts.FileWorker import *
-from scripts.Window import *
+import scripts
 import os
 
 if __name__ == '__main__':
@@ -12,6 +10,6 @@ if __name__ == '__main__':
             'examples_height':  3}
 
     path = os.path.dirname(os.path.abspath(__file__))
-    network = NetworkART1(conf)
-    worker = FileWorker(os.path.join(path, './resources/arrays.npz'), conf)
-    ui = Window(network, worker, conf)
+    network = scripts.NetworkART1(conf)
+    worker = scripts.FileWorker(os.path.join(path, './resources/arrays.npz'), conf)
+    ui = scripts.Window(network, worker, conf)
---
+++ scripts/__init__.py
@@ -0,0 +1,5 @@
+from scripts.NetworkART1 import *
+from scripts.FileWorker import *
+from scripts.Window import *
+
+__all__ = [NetworkART1, FileWorker, Window]
